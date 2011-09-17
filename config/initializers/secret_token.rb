9e557a60d7a62f58e4e3f011e3a1ebae2a107664
Tyler Singletary <tyler@klout.com>
2011-09-17 12:05:11 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenhuevos::Application.config.secret_token = 'e69f348dacc635b863d1e958a8b367d8c73bd2f48581fbca1b8737a41630cab2257943b4b3a648c6d3a1077b5d43dc5734488d9cca49b13ca5adba26e1897b7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rottenhuevos::Application.config.session_store :cookie_store, key: '_rottenhuevos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rottenhuevos::Application.config.session_store :active_record_store
